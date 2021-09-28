3323908a8667a39d757e25c0a12bec963cecc55e
Opilane TTHK <opilane@tthk.ee>
2021-09-28 12:01:44 +0300

    1

---
+++ GuessingGameThreeTimes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GuessingGameThreeTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ LoginValidation/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LoginValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada kasutajatunnus
+            //programm küsib kasutajal sisestada parooli
+            //programm kontrollib, kas mõlemad, nii kasutajatunnus kui ka parool on õiged
+            //kui mõlemad, kasutajatunnus ja parool on õiged, programm kuvab konsoolis "Tere, Tulemast"
+            //kui kasutaja tunnus või parool vale siis programm kuvab "sisselogimine ebaõnnestus, proovi uuesti".
+            //kt: admin, pr:admin1234
+            //kasutajal on kolm katset
+
+            int i = 0;
+            while(i < 3)
+            {
+                Console.WriteLine("sisesta kasutajatunnus");
+                string userName = Console.ReadLine();
+                Console.WriteLine("sisesta parool");
+                string userPassword = Console.ReadLine();
+                if(userName == "admin" && userPassword == "admin1234")
+                {
+                    Console.WriteLine("Tere tulemast!");
+                    break;
+                }
+                else
+                {
+                    i = i + 1;
+                        Console.WriteLine($"Proovi uuesti. {3 - i} katset on jäänud.");
+                }
+
+            }
+
+            Console.WriteLine("Kena päeva");
+
+        }
+    }
+}
